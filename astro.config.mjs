1ae5ea16aa552aa070399f239b376099040ab2f1
donniedark0-max <juan.vilca18@outlook.com>
2024-08-28 11:22:53 -0500

    fix deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import vercel from '@astrojs/vercel';
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-	output: 'hybrid',
-  integrations: [tailwind()]
+  output: 'server',
+  integrations: [tailwind(), react()],
+  adapter: vercel(),
 });
